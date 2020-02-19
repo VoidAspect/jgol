3d412a18fd8f46fc3c2799cc8326a404616553fc
Mikhail Horbunov <miwagorbi@gmail.com>
2020-02-19 13:20:12 +0200

    move NextGen to ChunkedProgressStrategy

--- src/main/java/com/voidaspect/jgol/game/ChunkedProgressStrategy.java
+++ src/main/java/com/voidaspect/jgol/game/ChunkedProgressStrategy.java
@@ -3,6 +3,8 @@ package com.voidaspect.jgol.game;
 import com.voidaspect.jgol.grid.Grid;
 import com.voidaspect.jgol.listener.CellListener;
 
+import java.util.Arrays;
+
 abstract class ChunkedProgressStrategy implements ProgressStrategy {
 
     final Grid grid;
@@ -14,7 +16,7 @@ abstract class ChunkedProgressStrategy implements ProgressStrategy {
     NextGen progressChunk(CellListener listener, int fromRow, int fromCol, int toRow, int toCol) {
         toRow = Math.min(grid.getColumns(), toRow);
         toCol = Math.min(grid.getRows(), toCol);
-        var ng = new NextGen(grid);
+        var ng = new NextGen();
         for (int row = fromRow; row < toRow; row++) {
             for (int col = fromCol; col < toCol; col++) {
                 int neighbors = grid.neighbors(row, col);
@@ -33,5 +35,92 @@ abstract class ChunkedProgressStrategy implements ProgressStrategy {
             }
         }
         return ng;
+    }
+
+    final class NextGen {
+
+        final CellBag spawned;
+
+        final CellBag died;
+
+        NextGen() {
+            this.spawned = new CellBag();
+            this.died = new CellBag();
+        }
+
+        public void willDie(int row, int col) {
+            died.add(row, col);
+        }
+
+        public void willSpawn(int row, int col) {
+            spawned.add(row, col);
+        }
+
+        void updateGrid() {
+            spawned.forEach((row, col) -> grid.set(row, col, true));
+            died.forEach((row, col) -> grid.set(row, col, false));
+        }
+    }
+
+    @FunctionalInterface
+    private interface CellOperation {
+        void apply(int row, int col);
+    }
+
+    /**
+     * An auxiliary data structure designed to hold a collection of cell indices of a 2-dimensional matrix.
+     * <br>Implemented as a Structure of Arrays with on-demand resizing.</br>
+     * <br>Arrays <b>r</b> and <b>c</b> hold row and column indices of cells respectively.</br>
+     * <p> API of {@code CellBag} allows:</p>
+     * <ol>
+     *     <li>to add cell indices one by one</li>
+     *     <li>to perform {@link CellOperation} on all cells</li>
+     * </ol>
+     */
+    private static final class CellBag {
+
+        private static final int MAX_ARRAY_LENGTH = Integer.MAX_VALUE - 8;
+
+        private static final int INITIAL_CAPACITY = 64;
+
+        private int[] r;
+
+        private int[] c;
+
+        private int size;
+
+        private int capacity;
+
+        public void forEach(CellOperation action) {
+            for (int i = 0; i < size; i++) {
+                action.apply(r[i], c[i]);
+            }
+        }
+
+        public void add(int row, int col) {
+            //region ensure capacity
+            if (size == 0) { // initial allocation
+                capacity = INITIAL_CAPACITY;
+                r = new int[INITIAL_CAPACITY];
+                c = new int[INITIAL_CAPACITY];
+            } else if (size == capacity) { // resize on-demand
+                if (size == MAX_ARRAY_LENGTH) {
+                    throw new IllegalStateException("cell bag too big");
+                }
+                // multiply by 1.5
+                int needed = capacity + (capacity >> 1);
+                if (needed < 0 || needed > MAX_ARRAY_LENGTH) { // handle overflow
+                    needed = MAX_ARRAY_LENGTH;
+                }
+                capacity = needed;
+                r = Arrays.copyOf(r, capacity);
+                c = Arrays.copyOf(c, capacity);
+            }
+            //endregion
+            int index = size++;
+            r[index] = row;
+            c[index] = col;
+        }
+
     }
 }
